8f348781553f319b0066653ecf75efae48257fa5
Phantom28S <faspo28@mail.ru>
2018-05-09 17:30:59 +0600

    com

--- bot.py
+++ bot.py
@@ -4,11 +4,11 @@ from users_repository import UsersRepository
 from states import State
 from users_repository import UserInfo
 
-from telebot import logger
-import time
+from flask import Flask, request
+import logging
 import os
 
-API_TOKEN = '562781455:AAGlciJk7H7KdgXUMkqU5V7zyGAoa4Epyz4'
+API_TOKEN = '411523498:AAGn-FBzFqXebb7IyJ-7KpkfHrJVVC0tJ6U'
 
 bot = telebot.TeleBot(API_TOKEN)
 users = UsersRepository("users")
@@ -99,15 +99,29 @@ def sends(message):
                                           'lizey8@mail.ru\n')
         handler.keyboard_in_tournaments(message)
 
-while True:
+# Здесь пишем наши хэндлеры
 
-    try:
+# Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
+if "HEROKU" in list(os.environ.keys()):
+    logger = telebot.logger
+    telebot.logger.setLevel(logging.INFO)
 
-        bot.polling(none_stop=True)
-
-    except ConnectionError as e:
-        logger.error(e)
-        time.sleep(15)
+    server = Flask(__name__)
+    @server.route("/bot", methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+    @server.route("/")
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url="https://botpls.herokuapp.com/") # этот url нужно заменить на url вашего Хероку приложения
+        return "?", 200
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+else:
+    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.
+    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
+    bot.remove_webhook()
+    bot.polling(none_stop=True)
 
 
 
